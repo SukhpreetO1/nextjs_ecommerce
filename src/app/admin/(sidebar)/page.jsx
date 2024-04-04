39b8b77eaa27093fabec6f2d62f075705a109e43
SukhpreetO1 <ssingh77022@gmail.com>
2024-04-04 22:19:37 +0530

    change in admin sidebar

--- src/app/admin/(sidebar)/page.jsx
+++ src/app/admin/(sidebar)/page.jsx
@@ -7,7 +7,7 @@ const AdminSidebar = () => {
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
-      if (sidebarOpen && !event.target.closest('#default-sidebar')) {
+      if (sidebarOpen && !event.target.closest('#admin_sidebar')) {
         setSidebarOpen(false);
       }
     };
@@ -23,15 +23,15 @@ const AdminSidebar = () => {
 
   return (
     <>
-      <div class="admin_sidebar sm:w-0 md:w-60 lg:w-60 xl:w-60 md:screen-lg:w-60">
-        <button onClick={toggleSidebar} data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
+      <div class="admin_side_bar sm:w-60">
+        <button onClick={toggleSidebar} data-drawer-target="admin_sidebar" data-drawer-toggle="admin_sidebar" aria-controls="admin_sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
           <span className="sr-only">Open sidebar</span>
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" >
             <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z" ></path>
           </svg>
         </button>
 
-        <aside id="default-sidebar" className={`fixed top-0 left-0 z-40 w-56 border-r-2 border-gray-300 h-screen transition-transform ${sidebarOpen ? '' : '-translate-x-full sm:translate-x-0'}`} aria-label="Sidebar" >
+        <aside id="admin_sidebar" className={`fixed top-0 left-0 z-40 w-56 border-r-2 border-gray-300 h-screen transition-transform ${sidebarOpen ? '' : '-translate-x-full sm:translate-x-0'}`} aria-label="Sidebar" >
           <div className="h-full py-4 overflow-y-auto bg-gray-50 dark:bg-white">
             <ul className="space-y-2 font-medium">
               <li>
