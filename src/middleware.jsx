e2b83332933f4839eedcd36c637fe1f9b5089e20
Sukhpreet01 <ssingh77022@gmail.com>
2024-07-31 17:32:41 +0530

    change in the middleware

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -3,14 +3,12 @@ import { FORGOT_PASSWORD, LOGIN_URL, SIGNUP_URL, USER_DASHBOARD, HOME_URL, ADMIN
 
 export function middleware(request) {
   const path = request.nextUrl.pathname;
-  console.log("error1");
 
   const isPublicPath = path === LOGIN_URL || path === SIGNUP_URL || path === FORGOT_PASSWORD;
 
   const token = request.cookies?.get('current_user_token');
   const admin_token = request.cookies?.get('current_admin_token');
   const super_admin_token = request.cookies?.get('current_super_admin_token');
-  console.log("error2");
   if (!(token || admin_token || super_admin_token) && !(isPublicPath || path === HOME_URL)) {
     return NextResponse.redirect(new URL(HOME_URL, request.url));
   }
@@ -20,7 +18,6 @@ export function middleware(request) {
   } else if (token && path === HOME_URL) {
     return NextResponse.redirect(new URL(USER_DASHBOARD, request.url));
   }
-  console.log("error3");
   if (admin_token && isPublicPath) {
     return NextResponse.redirect(new URL(ADMIN_DASHBOARD, request.url));
   } else if (admin_token && path === HOME_URL) {
@@ -32,7 +29,6 @@ export function middleware(request) {
   } else if (super_admin_token && path === HOME_URL) {
     return NextResponse.redirect(new URL(ADMIN_DASHBOARD, request.url));
   }
-  console.log("error4");
   return NextResponse.next();
 }
 
@@ -43,12 +39,11 @@ export const config = {
     '/node_modules/mongoose/**',
   ],
   matcher: [
-    "/",
     "/login",
     "/signup",
     "/forgot_password",
     "/dashboard",
     "/admin",
-    "/admin/:path",
+    "/admin/:path*",
   ],
 }
