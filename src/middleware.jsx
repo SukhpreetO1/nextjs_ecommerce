850735fd6147195c1064a028bd62122625f97848
Sukhpreet01 <ssingh77022@gmail.com>
2024-07-31 17:28:49 +0530

    change in the middleware

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -3,13 +3,14 @@ import { FORGOT_PASSWORD, LOGIN_URL, SIGNUP_URL, USER_DASHBOARD, HOME_URL, ADMIN
 
 export function middleware(request) {
   const path = request.nextUrl.pathname;
+  console.log("error1");
 
   const isPublicPath = path === LOGIN_URL || path === SIGNUP_URL || path === FORGOT_PASSWORD;
 
   const token = request.cookies?.get('current_user_token');
   const admin_token = request.cookies?.get('current_admin_token');
   const super_admin_token = request.cookies?.get('current_super_admin_token');
-
+  console.log("error2");
   if (!(token || admin_token || super_admin_token) && !(isPublicPath || path === HOME_URL)) {
     return NextResponse.redirect(new URL(HOME_URL, request.url));
   }
@@ -19,7 +20,7 @@ export function middleware(request) {
   } else if (token && path === HOME_URL) {
     return NextResponse.redirect(new URL(USER_DASHBOARD, request.url));
   }
-
+  console.log("error3");
   if (admin_token && isPublicPath) {
     return NextResponse.redirect(new URL(ADMIN_DASHBOARD, request.url));
   } else if (admin_token && path === HOME_URL) {
@@ -31,6 +32,8 @@ export function middleware(request) {
   } else if (super_admin_token && path === HOME_URL) {
     return NextResponse.redirect(new URL(ADMIN_DASHBOARD, request.url));
   }
+  console.log("error4");
+  return NextResponse.next();
 }
 
 export const config = {
@@ -45,18 +48,7 @@ export const config = {
     "/signup",
     "/forgot_password",
     "/dashboard",
-
     "/admin",
-    "/admin/dashboard",
-    "/admin/user_details",
-    "/admin/category_heading",
-    "/admin/category_heading/add_category_heading",
-    "/admin/category_heading/edit_category_heading/:id",
-    "/admin/category_types",
-    "/admin/category_types/add_category_types",
-    "/admin/category_types/edit_category_types/:id",
-    "/admin/categories",
-    "/admin/categories/add_categories",
-    "/admin/categories/edit_categories/:id",
+    "/admin/**",
   ],
 }
